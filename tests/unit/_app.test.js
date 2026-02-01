336fbe301cffa8a85ac4138de0b70e3c8b27ed54
Josuel-Lopes <josuel.lopes@maxcode42.com.br>
2026-02-01 16:07:10 -0300

    build: adjust docker services and database configuration

---
+++ tests/unit/_app.test.js
@@ -0,0 +1,7 @@
+describe("Application smoke test", () => {
+  describe("Anonymous user", () => {
+    test("should run the application environment", () => {
+      expect(true).toBe(true);
+    });
+  });
+});
