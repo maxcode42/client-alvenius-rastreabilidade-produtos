7d41e5e68c4ce44e01ae13b525a339439bc2279a
Josuel-Lopes <josuel.lopes@maxcode42.com.br>
2026-04-14 18:15:11 -0300

    test: create a test to prevent receining `POST` method requests on the `api/v1/status` endpoint

---
+++ tests/integration/api/v1/status/post.test.js
@@ -0,0 +1,30 @@
+import { STATUS_CODE } from "types/status-code";
+
+beforeAll(async () => {
+  console.log(">> BEFOREALL");
+});
+
+describe("POST'/api/v1/status'", () => {
+  describe("Anonymous user", () => {
+    test("Retrieving current system status", async () => {
+      const response = await fetch(
+        `${process.env.API_BASE_URL}/api/v1/status`,
+        {
+          method: "POST",
+        },
+      );
+
+      expect(response.status).toBe(STATUS_CODE.NOT_ALLOWED);
+
+      const responseBody = await response.json();
+
+      expect(responseBody).toEqual({
+        name: "MethodNotAllowedError",
+        message: "Método não permitido para este endpoint.",
+        action:
+          "Verifique se o método `HTTP` enviado é válido para esse endpoint",
+        status_code: STATUS_CODE.NOT_ALLOWED,
+      });
+    });
+  });
+});
